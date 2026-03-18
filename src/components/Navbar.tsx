896136c70f28c6ee123e369d508d90d34d3782e7
IgorD-lab <igordobricc@gmail.com>
2026-03-18 12:44:20 +0100

    initial styling and asset setup + navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import dayjs from "dayjs"
+
+import { NAV_ICONS, NAV_LINKS } from "#constants"
+
+const Navbar = () => {
+  
+  return (
+    <nav>
+      <div>
+        <img src="images/logo.svg" alt="logo"/>
+        <p className="font-bold">Y2K's Portfolio</p>
+
+        <ul>
+          {/* Map data into basic nav links turning them into jsx. Move data outside of component into a constant at the top of the file if it becomes unredable. */}
+          { NAV_LINKS.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          { NAV_ICONS.map(({ id, img }) =>
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          )}
+        </ul>
+
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/vite-env.d.ts
@@ -0,0 +1,5 @@
+// This file provides TypeScript type definitions for Vite-specific features.
+// It allows you to import CSS, images, and SVG files without errors, 
+// and gives you autocomplete for Vite's 'import.meta.env' variables.
+// ?temp fixes import './index.css' error inside main.tsx
+/// <reference types="vite/client" />
